38c45a8b7652b0a9850a3728eee3354972667050
Romas <r2udis@gmail.com>
2024-12-19 17:48:48 +0000

    add all car parts page

--- app/controllers/car_parts_controller.rb
+++ app/controllers/car_parts_controller.rb
@@ -1,4 +1,11 @@
 class CarPartsController < ApplicationController
+  def index
+    car_parts = CarPart.where(active: true)
+    @q = car_parts.ransack(params[:q])
+    @pagy, @car_parts = pagy(@q.result(distinct: true).includes(:part_type))
+
+    @part_types = PartType.all
+  end
   def show
     @car_part = CarPart.find(params[:id])
   end
--- app/controllers/part_types_controller.rb
+++ app/controllers/part_types_controller.rb
@@ -1,7 +1,7 @@
 class PartTypesController < ApplicationController
   def show
     @part_type = PartType.find(params[:id])
-    part_types = @part_type.car_parts
+    part_types = @part_type.car_parts.where(active: true)
     @q = part_types.ransack(params[:q])
     @pagy, @car_parts = pagy(@q.result(distinct: true))
   end
